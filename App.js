4da2e448bb97505876250b5e933c7c35d39b3fbe
CogniliumDev <cogniliumdev@gmail.com>
2022-12-29 03:21:07 +0500

    adding accordian feature 1

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './src/navigation/Navigation';
 import { store } from './src/store/store.js'
 import { Provider } from 'react-redux'
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigation />
+      <PaperProvider>
+        <Navigation />
+      </PaperProvider>
     </Provider>
   );
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,14 +1,19 @@
 import { View, Text, Animated, TouchableOpacity, Pressable } from "react-native";
 import { useState, useRef, useEffect } from "react";
+import { List } from 'react-native-paper';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
 
 const SideBar = ({ handelToggleSidebar }) => {
     const [show, setShow] = useState(false);
 
-    const fadeAnime = useRef(new Animated.Value(-500)).current;
+
+    const fadeAnime = useRef(new Animated.Value(-900)).current;
 
     const toggleSidebar = () => {
         Animated.timing(fadeAnime, {
-            toValue: show ? 0 : -500,
+            toValue: show ? 0 : -900,
             duration: 200, // ms
             useNativeDriver: true
         }).start();
@@ -20,11 +25,63 @@ const SideBar = ({ handelToggleSidebar }) => {
         toggleSidebar();
     }, [handelToggleSidebar]);
 
+    const arr = [
+        {
+            title: "Electronic Devices",
+            subCategory1: [
+                {
+                    title: "Smart Phones",
+                    subCategory2: [
+                        { title: "nokia mobiles" },
+                        { title: "redmi mobiles" },
+                        { title: "infinix mobiles" },
+                    ]
+                },
+                {
+                    title: "Laptops",
+                    subCategory2: [
+                        { title: "refurbished laptops" },
+                        { title: "traditional laptops" },
+                    ]
+                },
+                {
+                    title: "Smart Watches",
+                    subCategory2: [
+                        { title: "smart watches" },
+                    ]
+                },
+
+            ]
+        },
+        {
+            title: "Health & Beauty",
+            subCategory1: [
+                {
+                    title: "Fragrances",
+                    subCategory2: [
+                        { title: "women fragrances" },
+                        { title: "men fragrances" },
+                        { title: "unisex" },
+                    ]
+                },
+                {
+                    title: "Makeup",
+                    subCategory2: [
+                        { title: "foundation" },
+                        { title: "makeup accessories" },
+                        { title: "lips" },
+                    ]
+                },
+
+            ]
+        },
+    ]
+
 
     return (
         <>
             <Animated.View
-                className={`absolute top-0 left-0 z-50 w-[60%] h-full bg-slate-50 shadow-2xl`}
+                className={`absolute top-0 left-0 z-50 w-[70%] h-full bg-slate-50 shadow-2xl`}
                 style={{
                     transform: [{
                         // translateX: show ? 0 : -290
@@ -32,12 +89,69 @@ const SideBar = ({ handelToggleSidebar }) => {
                     }]
                 }}
             >
-                <TouchableOpacity className="p-3"
-                    onPress={toggleSidebar}
-                >
-                    <Text>X</Text>
-                </TouchableOpacity>
+                <View className="flex-row justify-between items-center px-2 mt-5">
+                    <Text className="text-blue-500 text-lg font-bold">
+                        CATEGORIES
+                    </Text>
+                    <TouchableOpacity className="p-3 bg-orange-200 rounded-md"
+                        onPress={toggleSidebar}
+                    >
+                        <FontAwesome name="close" color={"black"} size={20} />
 
+                    </TouchableOpacity>
+                </View>
+
+
+                <List.Section>
+
+                    {/* first level */}
+                    {arr?.map((category, index) => {
+                        return (
+                            <List.Accordion
+                                key={index}
+                                title={category.title}
+                                style={() => {
+                                    return { backgroundColor: "black" };
+                                }}
+                                left={props => <List.Icon {...props} color="blue" icon="face-woman" />}
+                            >
+                                {/* Second Level  */}
+                                {category?.subCategory1?.map((subCategory1, index) => {
+                                    return (
+                                        <List.Accordion
+                                            key={index}
+                                            title={subCategory1.title}
+                                            left={props => <List.Icon {...props} color="blue" icon="plus-thick" />}
+                                        >
+                                            {/* Third Level  */}
+                                            {subCategory1?.subCategory2?.map((subCategory2, index) => {
+                                                return (
+                                                    <List.Item
+                                                        left={props => <Text></Text>}
+                                                        key={index}
+                                                        title={subCategory2.title}
+                                                    />
+                                                );
+                                            })
+
+                                            }
+                                        </List.Accordion>
+                                    )
+                                })
+
+                                }
+
+
+                            </List.Accordion>
+                        );
+                    }
+                    )}
+
+                </List.Section>
+
+
+
+                {/* RIGHT BAR  */}
                 <Pressable
                     onPress={toggleSidebar}
                     className="absolute top-0 -right-[67%] w-[67%] h-full"
